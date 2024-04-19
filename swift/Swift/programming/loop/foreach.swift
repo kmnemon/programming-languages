15347db3899bd206643d7827df334ec8b39f4dfa
kmnemon <kmnemon@outlook.com>
2024-04-19 08:39:14 +0800

    rename project

---
+++ swift/Swift/programming/loop/foreach.swift
@@ -0,0 +1,21 @@
+//
+//  foreach.swift
+//  swift
+//
+//  Created by ke on 4/18/24.
+//
+
+import Foundation
+
+func switchCase() {
+    let name = "ke"
+    
+    switch name {
+        case "bob":
+            print("bob")
+        case "ke":
+            print("ke")
+        default:
+            print("default")
+    }
+}
---
+++ swift/Swift/programmingTests/loop/foreachTests.swift
@@ -0,0 +1,32 @@
+//
+//  foreachTests.swift
+//  swift
+//
+//  Created by ke on 4/19/24.
+//
+
+import XCTest
+
+final class foreachTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSwichCase() throws {
+        switchCase()
+        XCTAssertEqual(true, true, "good")
+    }
+
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+
+}
