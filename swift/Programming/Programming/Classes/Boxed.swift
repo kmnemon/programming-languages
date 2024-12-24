9ed0dc5e9c70fe38818abe2dd63519ae2b1c5ead
kmnemon <kmnemon@outlook.com>
2024-12-24 19:35:07 +0800

    boxed type

---
+++ swift/Programming/Programming/Classes/Boxed.swift
@@ -0,0 +1,43 @@
+//
+//  Boxed.swift
+//  Programming
+//
+//  Created by ke on 2024/12/24.
+//
+
+
+
+
+protocol Drawable {
+    func draw()
+}
+
+struct Circle: Drawable {
+    func draw() {
+        print("Drawing a circle")
+    }
+}
+
+struct Rectangle: Drawable {
+    func draw() {
+        print("Drawing a rectangle")
+    }
+}
+
+func getDrawable() -> Drawable {
+    return Circle()
+}
+
+//it's the same meaning of the one above, it returns boxed type
+func getDrawable2() -> any Drawable {
+    return Circle()
+}
+
+
+
+
+
+
+func difference() {
+    let drawables: [any Drawable] = [Circle(), Rectangle()]
+}
