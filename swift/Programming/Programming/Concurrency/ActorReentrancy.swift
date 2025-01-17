000ed4efc94950ef4c7d68d9eca7d04ac750ffa4
kmnemon <kmnemon@outlook.com>
2025-01-17 18:40:39 +0800

    actor

--- swift/Programming/Programming/Concurrency/ActorReentrancy.swift
+++ swift/Programming/Programming/Concurrency/ActorReentrancy.swift
@@ -8,3 +8,24 @@
 import Foundation
 
 
+actor Player {
+    var name = "Anonymous"
+    var score = 0
+
+    func addToScore() {
+        Task {
+            score += 1
+            try? await Task.sleep(for: .seconds(1))
+            print("Score is now \(score)")
+        }
+    }
+}
+
+func ActorReentrancy() async {
+    let player = Player()
+    await player.addToScore()
+    await player.addToScore()
+    await player.addToScore()
+    
+    try? await Task.sleep(for: .seconds(1.1))
+}
