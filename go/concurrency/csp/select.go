420ef70855536a17293e4628fddae9096e78a220
kmnemon <kmnemon@outlook.com>
2025-02-05 11:13:00 +0800

    default select

--- go/concurrency/csp/select.go
+++ go/concurrency/csp/select.go
@@ -44,3 +44,20 @@ func defaultSelect() {
 		fmt.Printf("In default after %v\n\n", time.Since(start))
 	}
 }
+
+func deafultSelect2() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
