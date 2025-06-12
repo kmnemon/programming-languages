07a968a4b5e720346466a1e7d5f779e39eafa4f8
kmnemon <kmnemon@outlook.com>
2025-06-12 16:46:55 +0700

    plus

--- swift/Programming/Programming/FunctionalProgramming/Simplicity/CouponEmail/CouponEmail.swift
+++ swift/Programming/Programming/FunctionalProgramming/Simplicity/CouponEmail/CouponEmail.swift
@@ -10,7 +10,38 @@ struct Subscriber {
     var recCount: Int
 }
 
+struct Coupon {
+    var code: String
+    var rank: String
+}
+
+struct Message {
+    var from: String
+    var to: String
+    var subject: String
+    var body: String
+}
+
 func subCouponRank( _ subscriber: Subscriber) -> String {
     subscriber.recCount > 10 ? "best" : "good"
 }
 
+func selectCouponsByRank(_ coupons: [Coupon], _ rank: String) -> [String] {
+    var couponCodes: [String] = []
+    for coupon in coupons {
+        if coupon.rank == rank {
+            couponCodes.append(coupon.code)
+        }
+    }
+    return couponCodes
+}
+
+func emailForSubscriber(_ subscriber: Subscriber, _ goods: [String], _ bests: [String]) -> Message {
+    var rank = subCouponRank(subscriber)
+    
+    if rank == "best" {
+        return Message(from: "newsletters@example.com", to: subscriber.email, subject: "Your best weekly coupons inside", body: "Here are the best coupons: " + bests.joined(separator: ","))
+    } else {
+        return Message(from: "newsletters@example.com", to: subscriber.email, subject: "Your good weekly coupons inside", body: "Here are the best coupons: " + goods.joined(separator: ","))
+    }
+}
