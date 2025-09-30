88faa976386f66811db89bb0cb4bd0f07076a329
kmnemon <kmnemon@outlook.com>
2025-09-30 17:23:07 +0700

    array oper

--- swift/Programming/Programming/Collection/Array.swift
+++ swift/Programming/Programming/Collection/Array.swift
@@ -10,3 +10,51 @@ func arrayAppend() {
     a.append(4)
     a.append(contentsOf: [4, 5])
 }
+
+func arrayOper() {
+    let arr = [1, 2, 3]
+    
+    //1. iterate over
+    for x in arr {
+       print(x)
+    }
+    
+    //2. iterate over but the first element of array
+    for x in arr.dropFirst() {
+        print(x)
+    }
+    
+    //3. iterate over but the last five
+    for x in arr.dropLast(5) {
+        print(x)
+    }
+    
+    //4.number all the elements in an array
+    for (num, element) in arr.enumerated() {
+        print(num)
+        print(element)
+    }
+    
+    //5.iterate over indices and elements
+    for (index, element) in zip(arr.indices, arr) {
+        print(index)
+        print(element)
+    }
+    
+    //6. find the location of a specific element
+    if let idx = arr.firstIndex( where: {
+        someMatchingLogic($0)
+    }) {
+        print(idx)
+    }
+    
+    
+
+    
+}
+
+func someMatchingLogic(_ element: Int) -> Bool {
+    return true
+}
+
+
---
+++ swift/Programming/ProgrammingTests/Collection/ArrayTests.swift
@@ -0,0 +1,16 @@
+//
+//  ArrayTests.swift
+//  Programming
+//
+//  Created by ke Liu on 9/30/25.
+//
+ 
+import Testing
+//@testable import Programming
+
+
+@Test func arrayTest() {
+    let arr = [1, 2]
+    arr.reserveCapacity(<#T##minimumCapacity: Int##Int#>)
+//    print(arr[3])
+}
