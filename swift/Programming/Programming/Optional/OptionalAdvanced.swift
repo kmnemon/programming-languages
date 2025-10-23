bc96b05ba92c06b397cf1b386a570cc6bde63734
kmnemon <kmnemon@outlook.com>
2025-10-23 17:05:33 +0700

    optional

--- swift/Programming/Programming/Optional/OptionalAdvanced.swift
+++ swift/Programming/Programming/Optional/OptionalAdvanced.swift
@@ -9,4 +9,20 @@
 func OptionalWithMap() {
     let characters: [Character] = ["a", "b", "c"]
     let firstCharacter = characters.first.map { String($0) } //Optional("a")
+    
+    let characters2: [Character] = ["a", "b", "c"]
+    let firstCharacter2 = characters2.first.map { String($0) } //nil
 }
+
+func OptionalWithFlatMap() {
+    let numbers = ["1", "2"]
+    let i = numbers.first.map { Int($0) } //Optional(Optional(1)) i??
+    let i2 = numbers.first.flatMap { Int($0) } //Optional(1) i?
+    
+    //another implement
+    if let a = numbers.first, let b = Int(a) {
+        print(b)
+    }
+    
+}
+
