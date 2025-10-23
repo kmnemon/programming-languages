83804fcad57435027ba74c2502afb595d3ad6b63
kmnemon <kmnemon@outlook.com>
2025-10-23 17:28:11 +0700

    pm

--- swift/Programming/Programming/PatternMatch/PatternMatch.swift
+++ swift/Programming/Programming/PatternMatch/PatternMatch.swift
@@ -11,3 +11,20 @@ func IfCase() {
         print("\(j) within range")
     }// 5 within range
 }
+
+func pickNonNil() {
+    let numbers = ["1", "2", "three"]
+    var sum = 0
+    
+    //1.pattern match
+    for case let i? in numbers.map({ Int($0)}) {
+        sum += i
+    }
+    
+    print(sum) //3
+    
+    //2.map reduce way
+    sum = numbers
+        .map { Int($0) }
+        .reduce(0) { $0 + ($1 ?? 0) }
+}
