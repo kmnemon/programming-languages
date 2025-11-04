a3de270df6a38ccde20150523fb42f8e8e0e4810
kmnemon <kmnemon@outlook.com>
2025-11-04 20:40:26 +0700

    stringbuilder

--- swift/Programming/Programming/FunctionalProgramming/SortCombination.swift
+++ swift/Programming/Programming/FunctionalProgramming/SortCombination.swift
@@ -14,11 +14,11 @@ struct Person2 {
 }
 
 let people = [
-Person2(first: "Emily", last: "Young", yearOfBirth: 2002),
-Person2(first: "David", last: "Gray", yearOfBirth: 1991),
-Person2(first: "Robert", last: "Barnes", yearOfBirth: 1985),
-Person2(first: "Ava", last: "Barnes", yearOfBirth: 2000),
-Person2(first: "Joanne", last: "Miller", yearOfBirth: 1994),
+    Person2(first: "Emily", last: "Young", yearOfBirth: 2002),
+    Person2(first: "David", last: "Gray", yearOfBirth: 1991),
+    Person2(first: "Robert", last: "Barnes", yearOfBirth: 1985),
+    Person2(first: "Ava", last: "Barnes", yearOfBirth: 2000),
+    Person2(first: "Joanne", last: "Miller", yearOfBirth: 1994),
 ]
 
 
@@ -53,9 +53,9 @@ func usage2() {
     let sortByFirstName: SortDescriptor<Person2> = .init ({ $0.first }, by: String.localizedStandardCompare )
     let sortByLastName: SortDescriptor<Person2> = .init ({ $0.last }, by: String.localizedStandardCompare )
     let sortByYear: SortDescriptor<Person2> = .init { $0.yearOfBirth }
-
+    
     let combined = sortByLastName.then(sortByFirstName).then(sortByYear)
     
     people.sorted(by: combined.areInIncreasingOrder)
-
+    
 }
---
+++ swift/Programming/Programming/ResultBuilder/StringBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  StringBuilder.swift
+//  Programming
+//
+//  Created by ke on 11/4/25.
+//
+
+@resultBuilder
+struct StringBuilder {
+    static func buildBlock() -> String {
+        return ""
+    }
+}
