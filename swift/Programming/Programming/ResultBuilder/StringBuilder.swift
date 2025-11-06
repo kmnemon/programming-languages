f01b78bbfbfa9a4053fdeff1ce85eef173f9a52f
kmnemon <kmnemon@outlook.com>
2025-11-06 08:34:42 +0700

    if else

--- swift/Programming/Programming/ResultBuilder/StringBuilder.swift
+++ swift/Programming/Programming/ResultBuilder/StringBuilder.swift
@@ -11,7 +11,7 @@ struct StringBuilder {
     static func buildBlock(_ strings: String...) -> String {
         strings.joined()
     }
-   
+    
     //2 buildBlock + buildExpression
     static func buildExpression(_ s: String) -> String {
         s
@@ -19,6 +19,27 @@ struct StringBuilder {
     static func buildExpression(_ x: Int) -> String {
         "\(x)"
     }
+    
+    //3 provide clearer compiler diagnostics
+    @available(*, unavailable,
+                message: "String Builders only support string and integer values")
+    static func buildExpression<A>(_ expression: A) -> String {
+        ""
+    }
+    
+    //4 support condition if
+    static func buildIf(_ s: String?) -> String {
+        s ?? ""
+    }
+    
+    //5 support condition if else
+    static func buildEither(first component: String) -> String {
+        component
+    }
+    static func buildEither(second component: String) -> String {
+        component
+    }
+    
 }
 
 //1
@@ -59,3 +80,74 @@ func greetEarth_rewritten() -> String {
     )
 }
 
+//4
+@StringBuilder func greet(planet: String) -> String {
+    "Hello, Planet"
+    if let idx = planets.firstIndex(of: planet) {
+        " "
+        idx
+    }
+    "!"
+}
+//greet(planet: "Earth") // Hello, Planet 2!
+//greet(planet: "Sun") // Hello, Planet!”
+
+func greet_rewritten(planet: String) -> String {
+    let v0 = "Hello, Planet"
+    var v1: String?
+    if let idx = planets.firstIndex(of: planet) {
+        v1 = StringBuilder.buildBlock(
+            StringBuilder.buildExpression(" "),
+            StringBuilder.buildExpression(idx)
+        )
+    }
+    let v2 = StringBuilder.buildIf(v1)
+    return StringBuilder.buildBlock(v0, v2)
+}
+
+//5
+@StringBuilder func greet2(planet: String) -> String {
+    "Hello, "
+    if let idx = planets.firstIndex(of: planet) {
+        switch idx {
+            case 2:
+                "World"
+            case 1, 3:
+                "Neighbor"
+            default:
+                "planet "
+                idx + 1
+        }
+    } else {
+        "unknown planet"
+    }
+    "!"
+}
+//greet2(planet: "Earth") // Hello, World!
+//greet2(planet: "Mars") // Hello, Neighbor!
+//greet2(planet: "Jupiter") // Hello, planet 5!
+//greet2(planet: "Pluto") // Hello, unknown planet!”
+
+func greet2_rewritten(planet: String) -> String {
+    let v0 = StringBuilder.buildExpression("Hello, ")
+    let v1: String
+    if let idx = planets.firstIndex(of: planet) {
+        let v1_0: String
+        switch idx {
+            case 2:
+                v1_0 = StringBuilder.buildEither(first: StringBuilder.buildBlock(StringBuilder.buildExpression("World")))
+            case 1, 3:
+                v1_0 = StringBuilder.buildEither(second: StringBuilder.buildEither(first: StringBuilder.buildBlock(StringBuilder.buildExpression("Neighbor"))))
+            default:
+                let v1_0_0 = StringBuilder.buildExpression("planet")
+                let v1_0_1 = StringBuilder.buildExpression(idx + 1)
+                
+                v1_0 = StringBuilder.buildEither(second: StringBuilder.buildEither(second: StringBuilder.buildBlock(v1_0_0, v1_0_1)))
+        }
+        v1 = StringBuilder.buildEither(first: v1_0)
+    } else {
+        v1 = StringBuilder.buildEither(second: StringBuilder.buildBlock( StringBuilder.buildExpression("unknown planet")))
+    }
+    let v2 = StringBuilder.buildExpression("!")
+    return StringBuilder.buildBlock(v0, v1, v2)
+}
