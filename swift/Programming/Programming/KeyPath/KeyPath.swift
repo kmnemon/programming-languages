86e6318ef2a11363a42a2e6a2530a3adcb7550eb
kmnemon <kmnemon@outlook.com>
2025-11-10 16:34:42 +0700

    kp

--- swift/Programming/Programming/KeyPath/KeyPath.swift
+++ swift/Programming/Programming/KeyPath/KeyPath.swift
@@ -4,6 +4,13 @@
 //
 //  Created by ke Liu on 11/9/25.
 //
+/*
+ 1. AnyKeyPath similar to a function of type (Any) -> Any?
+ 2. PartialKeyPath<Source> is similar to a function of type(Source) -> Any?
+ 3. KeyPath<Source, Target> read-only, (Source) -> Target
+ 4. WritableKeyPath<Source, Target>, “WritableKeyPath. Because all properties that form this latter key path are mutable, the key path itself allows mutation of the underlying value"
+ 5. ReferenceWritableKeyPath<Source, Target>, is used with values that have reference semantics (classes), The difference in use is that a WriteableKeyPath requires the root value to be mutable, whereas the ReferenceWritableKeyPath doesn't
+ */
 
 struct Address {
     var street: String
